7a93c767bb5907bf3a3431cb2aee33061b47476a
Devon Hollowood <devonhollowood@gmail.com>
2015-10-05 11:46:06 +0200

    Implement Sudoku trait for Dynamic

--- src/sudoku/implementations/dynamic.rs
+++ src/sudoku/implementations/dynamic.rs
@@ -1,8 +1,9 @@
 //! Dynamically-sized sudoku
 
 use std::ops::{Index, IndexMut};
-use std::iter::IntoIterator;
 use super::super::position::Position;
+use super::super::group::Group;
+use super::super::sudoku_trait::Sudoku;
 
 /// An implementation of a sudoku puzzle which is dynamic in size, depending on its input
 pub struct Dynamic {
@@ -27,7 +28,7 @@ impl Dynamic {
             9  => 3,
             16 => 4,
             25 => 5,
-            _  => panic!("Unsupported puzzle size: {}x{}"),
+            _  => panic!("Unsupported puzzle size: {}x{}", side_len, side_len),
         };
         let box_height = side_len / box_width;
         Dynamic {
@@ -43,14 +44,60 @@ impl Dynamic {
 impl Index<Position> for Dynamic {
     type Output = char;
     fn index<'a>(&'a self, pos: Position) -> &'a Self::Output {
-        &self.data[pos.row + pos.col]
+        &self.data[pos.row*self.ncols + pos.col]
     }
 }
 
 impl IndexMut<Position> for Dynamic {
     fn index_mut<'a>(&'a mut self, pos: Position) -> &'a mut Self::Output {
-        &mut self.data[pos.row + pos.col]
+        &mut self.data[pos.row*self.ncols + pos.col]
     }
+}
+
+impl Sudoku for Dynamic {
+    fn get_row(&self, pos: Position) -> Group {
+        let begin = pos.row*self.ncols;
+        let end = (pos.row+1)*self.ncols;
+        Group::from(&self.data[begin..end])
+    }
+
+    fn get_col(&self, pos: Position) -> Group{
+        let mut elements = Vec::with_capacity(self.nrows);
+        for row in 0..self.nrows {
+            elements.push(self.data[row*self.ncols + pos.col]);
+        }
+        Group::from(&elements[..])
+    }
+
+    fn get_box(&self, pos: Position) -> Group{
+        let box_num = self.box_height*(pos.row/self.box_height) + (pos.col/self.box_width);
+        let boxes_per_row = self.ncols/self.box_width;
+        let top_left = (box_num/boxes_per_row)*self.box_height*self.ncols +
+                       (box_num%boxes_per_row)*self.box_width;
+        let mut elements = Vec::with_capacity(self.box_width*self.box_height);
+        for box_row in 0..self.box_height {
+            for box_col in 0..self.box_width {
+                elements.push(self.data[top_left + box_row*self.ncols + box_col]);
+            }
+        }
+        Group::from(&elements[..])
+    }
+
+    fn allowed_characters(&self) -> &'static str {
+        match self.nrows {
+             4 => "1234",
+             6 => "123456",
+             9 => "123456789",
+            16 => "1234567890ABCDEF",
+            25 => "1234567890ABCDEFGHIJKLMNO",
+             _ => panic!("Unsupported puzzle size: {}x{}", self.nrows, self.nrows),
+        }
+    }
+
+    fn groups(&self, pos: Position) -> [Group; 3] {
+        [self.get_row(pos), self.get_col(pos), self.get_box(pos)]
+    }
+
 }
 
 #[cfg(test)]
